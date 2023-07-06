27d272eedc4c2856527032fd765940e7d2ba3f61
AVillan <anthony.r.villan@gmail.com>
2023-07-06 16:29:19 -0700

    basic google sheets read and process

--- Python-Sandbox/Sandbox.py
+++ Python-Sandbox/Sandbox.py
@@ -1,1 +1,8 @@
 print("hello world")
+
+import pandas as pd
+SHEET_ID = "1-Nv4EVXuKzzkDVYdXzpeiDxHfj3EFypvkJnsnOqW2zk"
+SHEET_NAME = "Sheet1"
+url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+df = pd.read_csv(url)
+print(df.head())
