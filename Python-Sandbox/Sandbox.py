388a7ebb991d1f7aa8963349a72a8799d8698bce
AVillan <anthony.r.villan@gmail.com>
2023-07-25 18:14:22 -0700

    Tabloo experimentation

--- Python-Sandbox/Sandbox.py
+++ Python-Sandbox/Sandbox.py
@@ -1,8 +1,15 @@
-print("hello world")
+# print("hello world")
 
 import pandas as pd
+import tabloo
+
 SHEET_ID = "1-Nv4EVXuKzzkDVYdXzpeiDxHfj3EFypvkJnsnOqW2zk"
 SHEET_NAME = "Sheet1"
 url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 df = pd.read_csv(url)
-print(df.head())
+
+# naiive printing of the panda dataframe
+# print(df.head())
+
+# tabloo experimentation
+tabloo.show(df)
